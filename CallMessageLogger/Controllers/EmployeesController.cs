02c0c407c7aeab1038f2227198833cfb4e97aee4
Casey Hudler <chudler624@gmail.com>
2022-11-10 15:06:07 -0500

    view fixed needs styling

--- CallMessageLogger/Controllers/EmployeesController.cs
+++ CallMessageLogger/Controllers/EmployeesController.cs
@@ -66,7 +66,7 @@ namespace CallMessageLogger.Controllers
 
                 };
 
-                return await Task.Run(() => View(viewModel));
+                return await Task.Run(() => View("View",viewModel));
             }
 
             return RedirectToAction("Index");
